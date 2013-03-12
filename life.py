b896ee6478c01f7f7c6ca01c12787dc6d6068125
Ben Alexander <ben.alexander92@gmail.com>
2013-03-12 17:38:38 -0500

    logic is done
    needs gui/graphical representation

--- life.py
+++ life.py
@@ -1,3 +1,9 @@
+# Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+# Any live cell with two or three live neighbours lives on to the next generation.
+# Any live cell with more than three live neighbours dies, as if by overcrowding.
+# Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+
 import sys, pygame
 
 pygame.init()
@@ -21,12 +27,12 @@ class Game(object):
 			for j in range(0,width):
 				self.board[(i,j)] = 0
 		
-	def print_board(self,d):
+	def print_board(self):
 		vis = []
 		for i in range(0,self.width):
 			vis.append([])
 			for j in range(0,self.width):
-				vis[i].append(d[(i,j)])
+				vis[i].append(self.board[(i,j)])
 			print vis[i]
 		print ''
 		
@@ -34,32 +40,49 @@ class Game(object):
 		self.board[(x,y)] = 1
 			
 	def live(self):
-		step = {}
-		
-		
-	def get_neighbors(self,b):
-		neighbors = {}
-		
-		for k in b:
-			count = 0
+	
+		def get_neighbors(b):
+			neighbors = {}
 			
-			for x in range(-1,2):
-				for y in range(-1,2):
-					try:
-						if b[((k[0]+x),(k[1]+y))] == 1 and not(x==0 and y==0):
-							count+=1
-					except KeyError:
-						pass
-			neighbors[k] = count
+			for k in b:
+				count = 0
+				
+				for x in range(-1,2):
+					for y in range(-1,2):
+						try:
+							if b[((k[0]+x),(k[1]+y))] == 1 and not(x==0 and y==0):
+								count+=1
+						except KeyError:
+							pass
+				neighbors[k] = count
+			
+			return neighbors
+	
+		step = {}
+		neighbors = get_neighbors(self.board)
+		for i in self.board:
+			if self.board[i]==1 and (neighbors[i] < 2 or neighbors[i] > 3):
+				step[i] = 0
+			elif self.board[i]==1 and neighbors[i] < 4:
+				step[i] = 1
+			elif self.board[i]==0 and neighbors[i] == 3:
+				step[i] = 1
+			else:
+				step[i] = 0
+		self.board = step
 		
-		return neighbors
+
 		
 life = Game(10)
 
-for i in range(0,3):
-	for j in range(0,3):
-		life.seed(i,j)
-life.print_board(life.get_neighbors(life.board))	
+for i in range(1,4):
+	life.seed(2,i+2)
+	
+life.print_board()
+
+for i in range(0,5):
+	life.live()
+	life.print_board()
 
 
 	
